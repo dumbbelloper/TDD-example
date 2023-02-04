2068bac632598db60d9f876f17392f6a53edbede
dumbbelloper <dumbbelloper@gmail.com>
2023-02-04 17:35:41 +0900

    상품 주문 Jpa 테스트

--- src/main/java/com/dumbbelloper/tdd/order/OrderRepository.java
+++ src/main/java/com/dumbbelloper/tdd/order/OrderRepository.java
@@ -1,18 +1,7 @@
 package com.dumbbelloper.tdd.order;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
+interface OrderRepository extends JpaRepository<Order, Long> {
 
-@Repository
-class OrderRepository {
-
-    private final Map<Long, Order> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(final Order order) {
-        order.assignId(++sequence);
-        persistence.put(order.getId(), order);
-    }
 }
